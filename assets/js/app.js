01063f05053f1eee98424de8a7405c8cc301ab84
Amaresh Maharana <amareshcoder@gmail.com>
2025-04-30 15:17:16 +0530

    Enhance profile section and styling; update theme toggle functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,44 @@
+"use strict";
+
 /**
- * @license MIT
- * @author codewithsadee <mohammadsadee24@gmail.com>
- * @copyright codewithsadee 2023
+ * Add event listener on multiple items
+ * @param {NodeList} $elements
+ * @param {String} eventType
+ * @param {Function} callback
  */
 
+const addEventOnElements = ($elements, eventType, callback) => {
+  for (const $item of $elements) {
+    $item.addEventListener(eventType, callback);
+  }
+};
 
-'use strict';
+
+/**
+ * Header scroll state
+ */
+
+const $header = document.querySelector('[data-header]');
+
+window.addEventListener('scroll', () => {
+    $header.classList[window.scrollY > 50 ? 'add' : 'remove']('active');
+});
+
+
+/**
+ * Search toggle
+ */
+
+const $searchToggler = document.querySelector('[data-search-toggler]');
+const $searchField = document.querySelector('[data-search-field]');
+
+let isExpanded = false;
+
+$searchToggler.addEventListener('click', () => {
+    $header.classList.toggle('search-active');
+    isExpanded = isExpanded ? false : true;
+    $searchToggler.setAttribute('aria-expanded', isExpanded);
+    $searchField.focus();
+});
+
+
--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,8 +1,28 @@
-/**
- * @license MIT
- * @author codewithsadee <mohammadsadee24@gmail.com>
- * @copyright codewithsadee 2023
- */
+"use strict";
 
+const /** {NodeList} */ $HTML = document.documentElement;
+const /** {Element} */ isDark = window.matchMedia(
+    "(prefers-color-scheme: dark)"
+  ).matches;
 
-'use strict';
+if (sessionStorage.getItem("theme")) {
+  $HTML.dataset.theme = sessionStorage.getItem("theme");
+} else {
+  $HTML.dataset.theme = isDark ? "dark" : "light";
+}
+
+let /** {Boolean} */ isPressed = false;
+
+const changeTheme = (event) => {
+    isPressed = isPressed ? false : true;
+    event.target.setAttribute('aria-pressed', isPressed);
+
+    $HTML.setAttribute('data-theme', ($HTML.dataset.theme === 'light') ? 'dark' : 'light');
+    sessionStorage.setItem('theme', $HTML.dataset.theme);
+}
+
+window.addEventListener("load", () => {
+  const /** {Element} */ $themeBtn = document.querySelector('[data-theme-btn]');
+
+  $themeBtn.addEventListener("click", changeTheme);
+});
